602a22bb9f05c2093a092381672bf1b4828999ec
sabbirtanvir13 <sabbirtanvirahmed18@gmail.com>
2026-07-23 12:56:39 +0600

    seo on my protfolio

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-// import { Outlet } from 'react-router';
-// import Footer from '../components/Footer';
-
-// const HomeLayout = () => {
-//     return (
-//         <div>
-//             <nav>
-//                 <Navbar></Navbar>
-//             </nav>
-//             <main>
-//                 <Outlet></Outlet>
-
-//             </main>
-
-//             <footer>
-//                <Footer></Footer>
-//             </footer>
-//         </div>
-//     );
-// };
-
-// export default HomeLayout;
 
 
 import React, { useEffect } from 'react';
